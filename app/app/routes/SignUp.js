53fd844d63ef483c9c1614f4f1d31fd5975733bd
shoaib11@student.ubc.ca <shoaib11@student.ubc.ca>
2023-11-09 18:17:05 -0800

    Removed header causing issues with response from route

--- app/app/routes/SignUp.js
+++ app/app/routes/SignUp.js
@@ -4,14 +4,7 @@ const db = require("../db-connection.js");
 import { HmacSHA256 } from "crypto-js";
 
 router.post("/api", async (req, res) => {
-  // ./createAProfile/api will utilize this route. /api isn't necessary, but it makes it apparent that this is a route call.
   console.log(req.body);
-
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
 
   const { name, email, username, password, isorganization } = req.body;
   try {
@@ -21,8 +14,7 @@ router.post("/api", async (req, res) => {
         message: "Please enter all required fields",
       });
     }
-    let member = await db.one("INSERT INTO member VALUES($1, $2, $3, $4, $5)", [
-      [
+    let member = await db.none("INSERT INTO member VALUES($1, $2, $3, $4, $5)", [
         name,
         email,
         username,
@@ -30,9 +22,8 @@ router.post("/api", async (req, res) => {
           password,
           "230e6fc32123b6164d3aaf26271bb1843c67193132c78137135d0d8f2160d1d3"
         ),
-        isorganization,
-      ],
-    ]);
+        isorganization
+      ]);
     res.json({ data: member, status: 201, message: "User SignUp Successful" });
   } catch (error) {
     res.json({ status: 501, message: "Failed to Add User" });
