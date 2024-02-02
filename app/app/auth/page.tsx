b95898f52ddc3a8ea0a0fb940daa49ddd66901f4
Satanshu Mishra <satanshumishra@outlook.com>
2024-02-01 21:14:00 -0800

    Further Changes to User Auth Pages
    - Removed Navigation Bar and added Website Logo w/o any additional links.
    - Added Checkbox to meet terms and conditions and Check to ensure it has been checked before form submission.

--- app/app/auth/page.tsx
+++ app/app/auth/page.tsx
@@ -24,7 +24,8 @@ export default function UserAuthentication({ params }: userauthprops) {
 	const searchParams = useSearchParams();
 
 	const [showSignIn, setShowSignIn] = useState(true);
-	const [terms, setTerms] = useState(true);
+	const [terms, setTerms] = useState(false);
+	const [highlightTerms, setHighlightTerms] = useState(false);
 
 	useEffect(() => {
 		if (searchParams.has("signin")) {
@@ -36,6 +37,10 @@ export default function UserAuthentication({ params }: userauthprops) {
 			setShowSignIn(false);
 		}
 	}, []);
+
+	useEffect(() => {
+		console.log("Terms: " + terms);
+	}, [terms]);
 
 	// FORMIK LOGIC
 
@@ -135,6 +140,7 @@ export default function UserAuthentication({ params }: userauthprops) {
 	}) => {
 		try {
 			if (!terms) {
+				setHighlightTerms(true);
 				throw new Error("Please accept the terms and conditions");
 			}
 
@@ -187,234 +193,242 @@ export default function UserAuthentication({ params }: userauthprops) {
 
 	return (
 		<>
-		<section className="z-50 w-screen p-10 flex flex-row justify-between items-center text-[1rem]">
+			<section className="z-50 w-screen p-10 flex flex-row justify-between items-center text-[1rem]">
 				<Link href={"./"}>
 					<Image
 						src={Logo}
 						alt={"TheSomethingSomethingCompany"}
 					></Image>
 				</Link>
-		</section>
-		{showSignIn ? (
-		<section className="grid grid-cols-1 tablet:grid-cols-2 p-0 m-0 w-full h-[50rem]">
-			<div className="flex flex-col justify-center items-start py-0 px-20 w-full h-full">
-				<h1 className="font-bold text-6xl mb-4">Sign In</h1>
-				<form
-					className="flex flex-col items-start my-4 w-full"
-					onSubmit={formikSignIn.handleSubmit}
-				>
-					<Input
-						label="Username"
-						type="text"
-						id="identifier"
-						name="identifier"
-						placeholder={formikSignIn.values.identifier}
-						onChange={formikSignIn.handleChange}
-						onBlur={formikSignIn.handleBlur}
-					/>
-					<Input
-						label="Password"
-						type="password"
-						id="password"
-						name="password"
-						placeholder={formikSignIn.values.password}
-						onChange={formikSignIn.handleChange}
-						onBlur={formikSignIn.handleBlur}
-					/>
-					<div className="w-full flex justify-between items-end">
-						<button
-							className="h-fit my-2 p-2 text-xl font-normal rounded-lg bg-black border-2 border-black hover:bg-white text-white hover:text-black transition-all duration-300 ease-in-out"
-							type="submit"
+			</section>
+			{showSignIn ? (
+				<section className="grid grid-cols-1 tablet:grid-cols-2 p-0 m-0 w-full h-[40rem]">
+					<div className="flex flex-col justify-center items-start py-0 px-20 w-full h-full">
+						<h1 className="font-bold text-6xl mb-4">Sign In</h1>
+						<form
+							className="flex flex-col items-start my-4 w-full"
+							onSubmit={formikSignIn.handleSubmit}
 						>
-							Sign In
-						</button>
-						<div className="flex flex-col items-end">
-							<p className="text-xl font-normal pb-2">
-								Not a member?
-							</p>
-							<button
-								className="h-fit p-2 my-2 text-xl font-normal rounded-lg bg-black border-2 border-black hover:bg-white text-white hover:text-black transition-all duration-300 ease-in-out"
-								onClick={() => setShowSignIn(!showSignIn)}
-							>
-								Sign Up
-							</button>
-						</div>
+							<Input
+								label="Username"
+								type="text"
+								id="identifier"
+								name="identifier"
+								placeholder={formikSignIn.values.identifier}
+								onChange={formikSignIn.handleChange}
+								onBlur={formikSignIn.handleBlur}
+							/>
+							<Input
+								label="Password"
+								type="password"
+								id="password"
+								name="password"
+								placeholder={formikSignIn.values.password}
+								onChange={formikSignIn.handleChange}
+								onBlur={formikSignIn.handleBlur}
+							/>
+							<div className="w-full flex flex-row-reverse justify-between items-end my-4">
+								<button
+									className="h-fit my-2 p-2 text-lg font-normal rounded-lg bg-black border-2 border-black hover:bg-white text-white hover:text-black transition-all duration-300 ease-in-out"
+									type="submit"
+								>
+									Sign In
+								</button>
+								<div className="flex flex-col items-start">
+									<p className="text-lg font-normal pb-2">
+										Not a member?
+									</p>
+									<button
+										className="h-fit p-2 my-2 text-lg font-normal rounded-lg bg-black border-2 border-black hover:bg-white text-white hover:text-black transition-all duration-300 ease-in-out"
+										onClick={() =>
+											setShowSignIn(!showSignIn)
+										}
+									>
+										Sign Up
+									</button>
+								</div>
+							</div>
+						</form>
 					</div>
-				</form>
-			</div>
-			<div className="hidden tablet:flex flex-col justify-center items-center p-20 w-full h-full bg-blue-600">
-				<div className="w-full">
-					<h1 className="text-6xl text-white font-black pb-4">
-						Welcome Back!
-					</h1>
-					<p className="text-3xl text-white font-normal pb-8">
-						Sign in to start connecting with people around the
-						world.
-					</p>
-				</div>
-				<Image
-					className="my-10"
-					src="/signinundraw.svg"
-					width={500}
-					height={500}
-					alt="signup"
-				/>
-			</div>
-		</section>
-	) : (
-		<section className="grid grid-cols-1 tablet:grid-cols-2 p-0 m-0 h-fit w-full">
-			<div className="hidden tablet:flex flex-col justify-center items-center p-20 w-fit h-full bg-green-600">
-				<div className="w-full">
-					<h1 className="text-6xl text-white font-black pb-4">
-						Join our Community!
-					</h1>
-					<p className="text-3xl text-white font-normal pb-8">
-						Connect with people around the world without worrying
-						about your privacy.
-					</p>
-				</div>
-				<Image
-					className="my-10"
-					src="/signupundraw.svg"
-					width={600}
-					height={600}
-					alt="signup"
-				/>
-			</div>
-			<div className="flex flex-col justify-center items-start p-20 w-full h-full">
-				<h1 className="font-bold text-6xl mb-4">Sign Up</h1>
-				<form
-					className="flex flex-col items-start my-4 w-full"
-					onSubmit={formik.handleSubmit}
-				>
-					<Input
-						label={
-							formik.touched.firstname && formik.errors.firstname
-								? formik.errors.firstname
-								: "Firstname"
-						}
-						type="text"
-						id="firstname"
-						name="firstname"
-						placeholder={formik.values.firstname}
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-					/>
-					<Input
-						label={
-							formik.touched.lastname && formik.errors.lastname
-								? formik.errors.lastname
-								: `Lastname`
-						}
-						type="text"
-						id="lastname"
-						name="lastname"
-						placeholder={formik.values.lastname}
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-					/>
-					<Input
-						label={
-							formik.touched.email && formik.errors.email
-								? formik.errors.email
-								: "Email"
-						}
-						type="email"
-						id="email"
-						name="email"
-						placeholder={formik.values.email}
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-					/>
-					<Input
-						label="Password"
-						type="password"
-						id="password"
-						name="password"
-						placeholder={formik.values.password}
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-					/>
-					{formik.touched.password && formik.errors.password ? (
-						<span className="mb-8 pb-2 text-xl font-semibold">
-							{formik.errors.password}
-						</span>
-					) : (
-						""
-					)}
-					<Input
-						label="Confirm Password"
-						type="confirmpassword"
-						id="confirmpassword"
-						name="confirmpassword"
-						placeholder={formik.values.confirmpassword}
-						onChange={formik.handleChange}
-						onBlur={formik.handleBlur}
-					/>
-					<div className="mb-4 items-top flex  space-x-2">
-						{/* <Checkbox id="organization" /> */}
-						<div className="grid gap-1.5 leading-none">
-							<label
-								htmlFor="organization"
-								className="text-xl font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-							>
-								Sign up as an Organization
-							</label>
-							<p className="text-xl text-muted-foreground">
-								Check this to sign up as an Organization.
+					<div className="hidden tablet:flex flex-col justify-center items-center p-20 w-full h-full bg-blue-600">
+						<div className="w-full">
+							<h1 className="text-6xl text-white font-black pb-4">
+								Welcome Back!
+							</h1>
+							<p className="text-3xl text-white font-normal pb-8">
+								Sign in to start connecting with people around
+								the world.
 							</p>
 						</div>
+						<Image
+							className="my-10"
+							src="/signinundraw.svg"
+							width={500}
+							height={500}
+							alt="signup"
+						/>
 					</div>
-					<div className="mb-4 items-top flex  space-x-2">
-						{/* <Checkbox
-              id="terms"
-              checked={terms}
-              onCheckedChange={(e) => setTerms(!terms)}
-            /> */}
-						<div className="grid gap-1.5 leading-none">
-							<label
-								htmlFor="terms"
-								className="text-xl font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-							>
-								Accept terms and conditions
-								<sup className="text-red-600">*</sup>
-							</label>
-							<p className="text-xl text-muted-foreground">
-								You agree to our{" "}
-								<a className="cursor-not-allowed">
-									Terms of Service
-								</a>{" "}
-								and{" "}
-								<a className="cursor-not-allowed">
-									Privacy Policy
-								</a>
-								.
+				</section>
+			) : (
+				<section className="grid grid-cols-1 tablet:grid-cols-2 p-0 m-0 h-fit w-full">
+					<div className="hidden tablet:flex flex-col justify-center items-center p-20 w-fit h-full bg-green-600">
+						<div className="w-full">
+							<h1 className="text-6xl text-white font-black pb-4">
+								Join our Community!
+							</h1>
+							<p className="text-3xl text-white font-normal pb-8">
+								Connect with people around the world without
+								worrying about your privacy.
 							</p>
 						</div>
+						<Image
+							className="my-10"
+							src="/signupundraw.svg"
+							width={600}
+							height={600}
+							alt="signup"
+						/>
 					</div>
-					<div className="w-full flex justify-between items-end">
-						<button
-							className="h-fit my-2 p-2 text-xl font-normal rounded-lg bg-black border-2 border-black hover:bg-white text-white hover:text-black transition-all duration-300 ease-in-out"
-							type="submit"
+					<div className="flex flex-col justify-center items-start p-20 w-full h-full">
+						<h1 className="font-bold text-6xl mb-4">Sign Up</h1>
+						<form
+							className="flex flex-col items-start my-4 w-full"
+							onSubmit={formik.handleSubmit}
 						>
-							Sign Up
-						</button>
-						<div className="flex flex-col items-end">
-							<p className="text-xl font-normal pb-2">
-								Already a member?
-							</p>
-							<button
-								className="h-fit p-2 my-2 text-xl font-normal rounded-lg bg-black border-2 border-black hover:bg-white text-white hover:text-black transition-all duration-300 ease-in-out"
-								onClick={() => setShowSignIn(!showSignIn)}
-							>
-								Sign In
-							</button>
-						</div>
+							<Input
+								label={
+									formik.touched.firstname &&
+									formik.errors.firstname
+										? formik.errors.firstname
+										: "Firstname"
+								}
+								type="text"
+								id="firstname"
+								name="firstname"
+								placeholder={formik.values.firstname}
+								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
+							/>
+							<Input
+								label={
+									formik.touched.lastname &&
+									formik.errors.lastname
+										? formik.errors.lastname
+										: `Lastname`
+								}
+								type="text"
+								id="lastname"
+								name="lastname"
+								placeholder={formik.values.lastname}
+								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
+							/>
+							<Input
+								label={
+									formik.touched.email && formik.errors.email
+										? formik.errors.email
+										: "Email"
+								}
+								type="email"
+								id="email"
+								name="email"
+								placeholder={formik.values.email}
+								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
+							/>
+							<Input
+								label="Password"
+								type="password"
+								id="password"
+								name="password"
+								placeholder={formik.values.password}
+								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
+							/>
+							{formik.touched.password &&
+							formik.errors.password ? (
+								<span className="mb-8 pb-2 text-xl font-semibold">
+									{formik.errors.password}
+								</span>
+							) : (
+								""
+							)}
+							<Input
+								label="Confirm Password"
+								type="confirmpassword"
+								id="confirmpassword"
+								name="confirmpassword"
+								placeholder={formik.values.confirmpassword}
+								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
+							/>
+							<div className="my-4 items-top flex  space-x-2">
+								{/* <Checkbox id="organization" /> */}
+								<div className="grid gap-1.5 leading-none">
+									<label
+										htmlFor="organization"
+										className="text-lg font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+									>
+										Sign up as an Organization
+									</label>
+									<p className="text-lg text-muted-foreground">
+										Check this to sign up as an
+										Organization.
+									</p>
+								</div>
+							</div>
+							<div className="mb-4 items-top flex  space-x-2">
+								<Checkbox
+									id="terms"
+									checked={terms}
+									onCheckedChange={(e) => setTerms(!terms)}
+								/>
+								<div className="grid gap-1.5 leading-none" style={{ "--highlightcolor" : highlightTerms ? "#FF0000" : "#000000"}}>
+									<label
+										htmlFor="terms"
+										className="text-lg text-[--highlightcolor] font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+									>
+										Accept terms and conditions
+										<sup className="text-red-600">*</sup>
+									</label>
+									<p className="text-lg text-muted-foreground">
+										You agree to our{" "}
+										<a className="cursor-not-allowed">
+											Terms of Service
+										</a>{" "}
+										and{" "}
+										<a className="cursor-not-allowed">
+											Privacy Policy
+										</a>
+										.
+									</p>
+								</div>
+							</div>
+							<div className="w-full flex flex-row-reverse justify-between items-end">
+								<button
+									className="h-fit my-2 p-2 text-lg font-normal rounded-lg bg-black border-2 border-black hover:bg-white text-white hover:text-black transition-all duration-300 ease-in-out"
+									type="submit"
+								>
+									Sign Up
+								</button>
+								<div className="flex flex-col items-start">
+									<p className="text-lg font-normal pb-2">
+										Already a member?
+									</p>
+									<button
+										className="h-fit p-2 my-2 text-lg font-normal rounded-lg bg-black border-2 border-black hover:bg-white text-white hover:text-black transition-all duration-300 ease-in-out"
+										onClick={() =>
+											setShowSignIn(!showSignIn)
+										}
+									>
+										Sign In
+									</button>
+								</div>
+							</div>
+						</form>
 					</div>
-				</form>
-			</div>
-		</section>
-	)}
-	</>
+				</section>
+			)}
+		</>
 	);
 }
